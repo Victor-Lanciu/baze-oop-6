80abbcf414a4ba3af068d9da728592af5b65ff02
Victor Lanciu <wlanciu@gmail.com>
2025-01-04 15:59:29 +0200

    Added homework

---
+++ HW/Angajat.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    public abstract class Angajat
+    {
+        public string Nume { get; set; }
+        public int Vechime { get; set; }
+        protected Angajat(string nume, int vechime)
+        {
+            Nume = nume;
+            Vechime = vechime;
+        }
+        public virtual decimal CalculeazaSalariu()
+        {
+            return 10000;
+        }
+
+    }
+    public class Manager : Angajat
+    {
+        public int Experienta { get; set; }
+        public Manager(string nume, int vechime, int experienta) : base(nume, vechime)
+        {
+            Experienta = experienta;
+        }
+        public override decimal CalculeazaSalariu()
+        {
+            decimal result = base.CalculeazaSalariu() + (decimal)0.3 * base.CalculeazaSalariu() * Experienta;
+            return result;
+        }
+    }
+    public class Programmer : Angajat
+    {
+        public List<string> Languages { get; set; }
+
+        public Programmer(string nume, int vechime, List<string> languages) : base(nume, vechime)
+        {
+            Languages = languages;
+        }
+        public override decimal CalculeazaSalariu()
+        {
+            decimal result = base.CalculeazaSalariu();
+            string Csharp = "C#";
+            bool knowCsharp = false;
+            if (Languages.Contains(Csharp))
+            {
+                result += base.CalculeazaSalariu() * 4;
+                knowCsharp = true;
+            }
+            foreach (string element in Languages)
+            {
+                if (element != Csharp)
+                {
+                    result += base.CalculeazaSalariu() * (decimal)0.5;
+
+                }
+            }
+            return result;
+        }
+    }
+    public class Contabil : Angajat
+    {
+        public int Experienta { get; set; }
+
+        public Contabil(string nume, int vechime, int experienta) : base(nume, vechime)
+        {
+            Experienta = experienta;
+        }
+        public override decimal CalculeazaSalariu()
+        {
+            decimal result = base.CalculeazaSalariu() + base.CalculeazaSalariu() * (decimal)0.2;
+            return result;
+        }
+    }
+
+}
---
+++ HW/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var angajati = new List<Angajat>
+            {
+                new Programmer("Gabrie", 3, ["C#" ,"JavaScript", "SQK"]),
+                new Programmer("Temur", 5, ["Java", "Kotlin"]),
+                new Manager("Ion", 3,8),
+                new Contabil("Alexandra", 4,5)
+            };
+            foreach (var angajat in angajati)
+            {
+                Console.WriteLine(angajat.CalculeazaSalariu());
+            }
+        }
+    }
+}
