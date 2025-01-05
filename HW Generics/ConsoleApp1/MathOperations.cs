7025357b0a84dd7f91b72b7e365a02ed19149cee
Victor Lanciu <wlanciu@gmail.com>
2025-01-05 22:45:44 +0200

    HW Generics

---
+++ HW Generics/ConsoleApp1/MathOperations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class MathOperations<T> where T : INumber<T>
+    {
+        public T Add(T a, T b)
+        {
+            return a + b;
+        }
+        public T Subtract(T a, T b)
+        {
+            return a - b;
+        }
+        public T Multiply(T a, T b)
+        {
+            return a * b;
+        }
+        public T Divide(T a, T b)
+        {
+            if (b == T.Zero)
+            {
+                throw new ArgumentException("Dividing by zero is undefined");
+            }
+            return a / b;
+        }
+
+
+    }
+}
---
+++ HW Generics/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations<int> intOperators = new MathOperations<int>();
+            Console.WriteLine(intOperators.Divide(15, 5));
+            Console.WriteLine(intOperators.Add(15, 5));
+
+            MathOperations<double> doubleOperators = new MathOperations<double>();
+            Console.WriteLine(doubleOperators.Multiply(2d, 5d));
+
+            MathOperations<float> floatOperation = new MathOperations<float>();
+            Console.WriteLine(floatOperation.Subtract(55f, 22f));
+        }
+    }
+}
